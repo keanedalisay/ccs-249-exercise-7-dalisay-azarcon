3a8b29d9153b7cdbb2d8cc8324d4ae45cb4f9e50
Keane Emanuel Dalisay <keandals21@gmail.com>
2025-05-13 14:59:05 +0800

    Add RNN-based named entity tagging code

--- ner/main.py
+++ ner/main.py
@@ -3,8 +3,17 @@ import re, ast
 from collections import defaultdict
 
 import pandas as pd
+import numpy as np
 
 from sklearn.metrics import precision_score, recall_score
+from sklearn.model_selection import train_test_split
+
+from tensorflow.keras import Sequential
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.layers import Embedding, SimpleRNN, Dense, TimeDistributed
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.metrics import Precision, Recall
+from tensorflow.keras.utils import to_categorical
 
 class HiddenMarkovModel:
   def __init__(self):
@@ -122,6 +131,50 @@ class HiddenMarkovModel:
         # Calculate the emission probability
         self.emission_probs[tag][word] = emissions[word] / self.tag_count[tag]
 
+class RNN:
+  def __init__(self, vocab_size, embedding_dim, rnn_units, num_tags):
+    self.model = Sequential()
+    self.model.add(Embedding(input_dim=vocab_size, output_dim=embedding_dim))
+    self.model.add(SimpleRNN(rnn_units, return_sequences=True))
+    self.model.add(TimeDistributed(Dense(num_tags, activation='softmax')))
+    self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=[Precision(), Recall()])
+
+  def train(self, X_train, y_train):
+    early_stopping = EarlyStopping(monitor='val_loss', patience=3)
+    self.model.fit(X_train, y_train, epochs=10, batch_size=32, validation_split=0.2, callbacks=[early_stopping])
+
+  def predict(self, X_test):
+    return self.model.predict(X_test)
+
+def sentence_tagger(sentences, tags):
+  word_count = {}
+  tag_count = {}
+  tagged_sentences = []
+
+  split_sentences = [sentence.split() for sentence in sentences]
+
+  for i in range(len(split_sentences)):
+      split_sentence = split_sentences[i]
+      sentence_tags = tags[i]
+      tagged_sentence = []
+      for j in range(len(split_sentence)):
+        if split_sentence[j] not in word_count:
+          word_count[split_sentence[j]] = 1
+        else:
+          word_count[split_sentence[j]] += 1
+
+        if sentence_tags[j] not in tag_count:
+          tag_count[sentence_tags[j]] = 1
+        else:
+          tag_count[sentence_tags[j]] += 1
+
+        if split_sentence[j] not in tagged_sentence:
+          tagged_sentence.append((split_sentence[j], sentence_tags[j]))
+
+      tagged_sentences.append(tagged_sentence)
+
+  return word_count, tag_count, tagged_sentences
+
 def main():
   # Uses the Named Entity Recognition (NER) Corpus at Kaggle 
   # Accessible at https://www.kaggle.com/datasets/naseralqaydeh/named-entity-recognition-ner-corpus/data
@@ -130,10 +183,10 @@ def main():
 
   sentences = df['Sentence'].to_list()
   split_sentences = [sentence.split() for sentence in sentences] 
-  ne_sentences = df['Tag'].apply(ast.literal_eval)
+  tags = df['Tag'].apply(ast.literal_eval)
 
   hmm = HiddenMarkovModel()
-  hmm.train(split_sentences, ne_sentences)
+  hmm.train(split_sentences, tags)
 
   test_sentence = ['Manila', 'is', 'the', 'capital', 'of', 'the', 'Philippines', '.', 'Maria', 'Villafuerte',',', 'a', 'Filipino', 'citizen', ',', 'is', 'a', 'student', 'at', 'the', 'University', 'of', 'the', 'Philippines']
   test_sentence_labels = ['B-geo', 'O', 'O', 'O', 'O', 'O', 'B-geo', 'O', 'B-per', 'B-per', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'B-org', 'I-org', 'I-org', 'I-org', 'I-org']
@@ -146,6 +199,27 @@ def main():
 
   print(f'\nPrecision: {precision_score(result, test_sentence_labels, average="micro", zero_division=0) * 100}%')
   print(f'Recall: {recall_score(result, test_sentence_labels, average="micro", zero_division=0) * 100}%\n')
+
+  # RNN Model
+  wc, tc, tagged_sentences = sentence_tagger(sentences, tags)
+  maxlen = max([len(s) for s in tagged_sentences])
+
+  words = list(set(wc.keys()))
+  tags = list(set(tc.keys()))
+
+  word2idx = {w: i for i, w in enumerate(words)}
+  tag2idx = {t: i for i, t in enumerate(tags)}
+
+  X = [[word2idx[w[0]] for w in s] for s in tagged_sentences]
+  X = sequence.pad_sequences(maxlen=maxlen, sequences=X, padding="post",value=len(words) - 1)
+
+  y = [[tag2idx[w[1]] for w in s] for s in tagged_sentences]
+  y = sequence.pad_sequences(maxlen=maxlen, sequences=y, padding="post", value=tag2idx["O"])
+  y = np.array([to_categorical(i, num_classes=len(tags)) for i in y])
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+
+  rnn = RNN(vocab_size=len(words), embedding_dim=64, rnn_units=64, num_tags=len(tags))
+  rnn.train(X_train, y_train)
 
 if __name__ == '__main__':
   main()
